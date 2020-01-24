bd9dff454c195947754b3cb626e067762a613e56
Teletha <Teletha.T@gmail.com>
2020-01-24 21:50:18 +0900

    Initial commit.

---
+++ src/project/java/Project.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 stoneforge Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+public class Project extends bee.api.Project {
+
+  {
+      product("com.github.teletha", "stoneforge", "1.0");
+  }
+}
