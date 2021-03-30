d781efe5bd9e53ff529f92924df324e8bfc7a803
Teletha <Teletha.T@gmail.com>
2021-03-30 15:49:19 +0900

    feat: Enable CI.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -10,7 +10,7 @@
 public class Project extends bee.api.Project {
 
     {
-        product("com.github.teletha", "stoneforge", "1.0");
+        product("com.github.teletha", "stoneforge", ref("version.txt"));
 
         require("com.github.teletha", "sinobu");
         require("com.github.teletha", "stylist");
