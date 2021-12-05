85a8e44c8f6fa3ef553b7a0cb5bc58b02aed9ba3
Teletha <Teletha.T@gmail.com>
2021-12-05 12:33:59 +0900

    chore: Add auto generated files to index.

--- src/main/java/javadng/parser/JavadocModel.java
+++ src/main/java/javadng/parser/JavadocModel.java
@@ -10,9 +10,8 @@
 package javadng.parser;
 
 import static javax.tools.Diagnostic.Kind.*;
-import static javax.tools.Diagnostic.Kind.OTHER;
-import static javax.tools.DocumentationTool.Location.*;
-import static javax.tools.JavaFileObject.Kind.*;
+import static javax.tools.DocumentationTool.Location.DOCUMENTATION_OUTPUT;
+import static javax.tools.JavaFileObject.Kind.SOURCE;
 import static javax.tools.StandardLocation.*;
 
 import java.awt.Desktop;
