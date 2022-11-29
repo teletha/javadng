f821ef9472f56d60b17e7fe2d93dd4a514b11f86
teletha <teletha@users.noreply.github.com>
2022-11-29 06:13:19 +0000

    deploy: fd5568f75a1e7bdeff7cb68af13cbfc636b84bbd

---
+++ root.js
@@ -0,0 +1,259 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"javadng",
+		"javadng.design",
+		"javadng.page",
+		"javadng.parser",
+		"javadng.repository",
+		"javadng.web"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "APIPage",
+			"packageName": "javadng.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ClassInfo",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "CodeRepository",
+			"packageName": "javadng.repository",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Data",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Data.Doc",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Design",
+			"packageName": "javadng.design",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DocumentInfo",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DocumentPage",
+			"packageName": "javadng.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DomPage",
+			"packageName": "javadng.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ExecutableInfo",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "FieldInfo",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "HTML",
+			"packageName": "javadng",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "JavadngStyleDSL",
+			"packageName": "javadng.design",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc",
+			"packageName": "javadng.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableAll",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableOutput",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableProduct",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableProject",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableSources",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableVersion",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.ÅssignableÅrbitrary",
+			"packageName": "javadng.page",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Javadoc.Ìnstantiator",
+			"packageName": "javadng.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "JavadocModel",
+			"packageName": "javadng.page",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "JavadocModel.Internal",
+			"packageName": "javadng.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MemberInfo",
+			"packageName": "javadng.parser",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MethodInfo",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Minify",
+			"packageName": "javadng.web",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Page",
+			"packageName": "javadng.page",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ParameterizableInfo",
+			"packageName": "javadng.parser",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "SampleInfo",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "SiteBuilder",
+			"packageName": "javadng",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Styles",
+			"packageName": "javadng.design",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TemplateStore",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TypeResolver",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Util",
+			"packageName": "javadng.parser",
+			"type": "Class"
+		}
+	]
+}
