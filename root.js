96e9e3f94dc71a99d0ede67536329c2a91feab03
teletha <teletha@users.noreply.github.com>
2022-12-03 07:27:57 +0000

    deploy: f2815d421d3b89da87cb1832735234ca938a1777

--- root.js
+++ root.js
@@ -20,13 +20,6 @@ const root = {
 		{
 			"modifiers": {
 				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "ActivityPage",
-			"packageName": "javadng.page",
-			"type": "Class"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
 			"name": "ClassInfo",
 			"packageName": "javadng.parser",
 			"type": "Class"
@@ -72,6 +65,13 @@ const root = {
 			"name": "DocumentPage",
 			"packageName": "javadng.page",
 			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DocumentProvider",
+			"packageName": "javadng.page",
+			"type": "Interface"
 		},
 		{
 			"modifiers": {
